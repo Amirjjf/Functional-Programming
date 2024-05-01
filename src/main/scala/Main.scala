c1c5ef1d044ff3c1c612a753ddbc1d803faf7b51
Trev-ops <152309481+Trev-ops@users.noreply.github.com>
2024-05-01 18:59:53 +0300

    Update Main.scala
    added so that the data is written to a .csv file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,23 +2,25 @@ import sttp.client3._
 import sttp.client3.circe._
 import io.circe.generic.auto._
 import io.circe.Error
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.io.PrintWriter
+import java.io.File
 
 case class TimeSeriesData(datasetId: Int, startTime: String, endTime: String, value: Double)
 case class ApiResponse(data: List[TimeSeriesData])
 
-object WindPowerForecast {
-  def main(args: Array[String]): Unit = {
-    val apiKey = "f2ace58435cb48b593e949b5f5515c0b"  // Your API key
-    val datasets = "245,247"  // Dataset IDs you want to query
-    val startDate = "2024-05-02T00:00:00Z"
-    val endDate = "2024-05-03T00:00:00Z"
-    val requestUrl = s"https://data.fingrid.fi/api/data?datasets=$datasets&startTime=$startDate&endTime=$endDate"
+object Main extends App {
+  val apiKey = "f2ace58435cb48b593e949b5f5515c0b"  // Your API key
+  val datasets = "245"  // Dataset IDs you want to query
+  val endDate = ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT)
+  val startDate = ZonedDateTime.now().minusDays(1).format(DateTimeFormatter.ISO_INSTANT)
+  val requestUrl = s"https://data.fingrid.fi/api/datasets/$datasets/data?startTime=$startDate&endTime=$endDate" // Doesnt get data for the entire day
 
-    val backend = HttpURLConnectionBackend()
+  val backend = HttpURLConnectionBackend()
 
-    val response = sendRequest(apiKey, requestUrl, backend)
-    handleResponse(response)
-  }
+  val response = sendRequest(apiKey, requestUrl, backend)
+  handleResponse(response)
 
   def sendRequest(apiKey: String, url: String, backend: SttpBackend[Identity, Any]): Response[Either[ResponseException[String, Error], ApiResponse]] = {
     basicRequest
@@ -31,7 +33,12 @@ object WindPowerForecast {
   def handleResponse(response: Response[Either[ResponseException[String, Error], ApiResponse]]): Unit = {
     response.body match {
       case Right(apiResponse) =>
-        apiResponse.data.foreach(data => println(s"Dataset ID: ${data.datasetId}, Start Time: ${data.startTime}, End Time: ${data.endTime}, Value: ${data.value} MW/h"))
+        val writer = new PrintWriter(new File("renewable_energy_data.csv"))
+        writer.write("Dataset ID,Start Time,End Time,Value MW/h\n") // Write the header
+        apiResponse.data.foreach { data =>
+          writer.write(s"${data.datasetId},${data.startTime},${data.endTime},${data.value}\n") // Write each line of data
+        }
+        writer.close()
       case Left(error) =>
         error match {
           case DeserializationException(body, e) =>
