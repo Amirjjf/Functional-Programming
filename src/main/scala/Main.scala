564f0ee9abf9a32c5625be66e2b9a679c3507278
Johannes <44120608+Kumole@users.noreply.github.com>
2024-05-05 14:21:04 +0300

    made more functional

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,94 +1,77 @@
+import io.circe.generic.auto._
+import org.jfree.chart.plot.PlotOrientation
+import org.jfree.chart.{ChartFactory, ChartUtils}
+import org.jfree.data.category.DefaultCategoryDataset
 import sttp.client3._
 import sttp.client3.circe._
-import io.circe.generic.auto._
-import io.circe.Error
+
 import java.io.{File, PrintWriter}
 import scala.io.Source
-import scala.io.StdIn.readLine
-import org.jfree.chart.{ChartFactory, ChartUtils}
-import org.jfree.chart.plot.PlotOrientation
-import org.jfree.data.category.DefaultCategoryDataset
+import scala.util.Try
 
 case class TimeSeriesData(datasetId: Int, startTime: String, endTime: String, value: Double)
 case class ApiResponse(data: List[TimeSeriesData], pagination: Option[Pagination])
 case class Pagination(currentPage: Int, lastPage: Int)
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    menuLoop() // Start the main menu loop
+object Main extends App {
+  val powerApi = new PowerAPI()
+  menuLoop()
 
-    def menuLoop(): Unit = {
-      println("1. Monitor/Control\n" +
-        "2. Collect data\n" +
-        "3. View energy generation and storage\n" +
-        "4. Analyse data\n" +
-        "5. Detect and handle issues\n" +
-        "0. Exit")
-      print("Enter selection: ")
-      val command = readLine()
-      command match {
-        case "1" =>
-          val data = PowerAPI.fetchData("245", "2024-05-01T00:00:00Z", "2024-05-02T00:00:00Z").data
-          PowerAPI.printData(data)
-          println()
-        case "2" =>
-          val data = PowerAPI.fetchData("245", "2024-05-01T00:00:00Z", "2024-05-02T00:00:00Z").data
-          PowerAPI.printData(data)
-          PowerAPI.storeData(data, "renewable_energy_data2.csv")
-          println()
-        case "3" =>
-          viewEnergyGenerationAndStorage()
-        case "4" =>
-        // Implementation for analyse data
-        case "5" =>
-          detectAndHandleIssues()
-        case "0" =>
-          return // Exit the menu loop
-        case _ =>
-          println("Unrecognized command. Please try again.")
-      }
-      menuLoop()
+  @annotation.tailrec
+  def menuLoop(): Unit = {
+    println("1. Monitor/Control\n" +
+      "2. Collect data\n" +
+      "3. View energy generation and storage\n" +
+      "4. Analyse data\n" +
+      "5. Detect and handle issues\n" +
+      "0. Exit")
+    print("Enter selection: ")
+    scala.io.StdIn.readLine() match {
+      case "1" =>
+        val data = powerApi.fetchData("245", "2024-05-01T00:00:00Z", "2024-05-02T00:00:00Z")
+        data.foreach(powerApi.printData)
+        println()
+      case "2" =>
+        val data = powerApi.fetchData("245", "2024-05-01T00:00:00Z", "2024-05-02T00:00:00Z")
+        data.foreach(powerApi.printData)
+        data.foreach(d => powerApi.storeData(d, "renewable_energy_data2.csv"))
+        println()
+      case "3" =>
+        viewEnergyGenerationAndStorage()
+      case "4" =>
+        println("Analyse data option selected.")
+      case "5" =>
+        detectAndHandleIssues()
+      case "0" =>
+        println("Exiting...")
+        return
+      case _ =>
+        println("Unrecognized command. Please try again.")
     }
-
+    menuLoop()
   }
 
   def viewEnergyGenerationAndStorage(): Unit = {
     println("Viewing energy generation and storage...")
-    val data = PowerAPI.readDataFromCSV("renewable_energy_data.csv")
-    PowerAPI.plotData(data)
+    powerApi.readDataFromCSV("renewable_energy_data.csv").foreach(powerApi.plotData)
   }
 
   def detectAndHandleIssues(): Unit = {
     println("Detecting and handling issues based on stored data...")
-    val data = PowerAPI.readDataFromCSV("renewable_energy_data.csv")
-    PowerAPI.detectIssues(data)
+    powerApi.readDataFromCSV("renewable_energy_data.csv").map(powerApi.detectIssues)
   }
 }
 
 // 181. Wind power production - real time data (3 min) (https://data.fingrid.fi/en/datasets/181)
 // 191. Hydro power production - real time data (3 min) (https://data.fingrid.fi/en/datasets/191)
 // 248. Solar power generation forecast - updates every 15 minutes (https://data.fingrid.fi/en/datasets/248)
-object PowerAPI {
-  def apiKey: String = "f2ace58435cb48b593e949b5f5515c0b"
-  def backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
-  def lowOutputThreshold: Double = 200.0  // Define threshold for low energy output detection
+class PowerAPI {
+  val apiKey: String = "f2ace58435cb48b593e949b5f5515c0b"
+  val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
 
-  def fetchData(datasets: String, startDate: String, endDate: String, currentPage: Int = 1, allData: List[TimeSeriesData] = List.empty[TimeSeriesData]): ApiResponse = {
+  def fetchData(datasets: String, startDate: String, endDate: String): Try[List[TimeSeriesData]] = {
     val baseUrl = s"https://data.fingrid.fi/api/data?datasets=$datasets&startTime=$startDate&endTime=$endDate&pageSize=10000"
-    val pagedUrl = s"$baseUrl&page=$currentPage"
-    val response = sendRequest(apiKey, pagedUrl, backend)
-    response.body match {
-      case Right(apiResponse) =>
-        val newData = allData ++ apiResponse.data
-        apiResponse.pagination match {
-          case Some(pagination) if currentPage < pagination.lastPage =>
-            fetchData(datasets, startDate, endDate, currentPage + 1, newData)
-          case _ =>
-            ApiResponse(newData, None)
-        }
-      case Left(_) =>
-        ApiResponse(List(), None)
-    }
+    sendRequest(apiKey, baseUrl, backend).map(_.data)
   }
 
   def printData(data: List[TimeSeriesData]): Unit = {
@@ -97,20 +80,20 @@ object PowerAPI {
 
   def storeData(data: List[TimeSeriesData], fileName: String): Unit = {
     val writer = new PrintWriter(new File(fileName))
-    writer.println("Dataset ID,Start Time,End Time,Value MW/h")
-    data.foreach { d =>
-      writer.println(s"${d.datasetId},${d.startTime},${d.endTime},${d.value}")
+    try {
+      writer.println("Dataset ID,Start Time,End Time,Value MW/h")
+      data.foreach(d => writer.println(s"${d.datasetId},${d.startTime},${d.endTime},${d.value}"))
+    } finally {
+      writer.close()
     }
-    writer.close()
-    println("Data has been stored to the CSV file.")
   }
 
-  def readDataFromCSV(filePath: String): List[TimeSeriesData] = {
+  def readDataFromCSV(filePath: String): Try[List[TimeSeriesData]] = Try {
     val source = Source.fromFile(filePath)
     try {
       source.getLines().drop(1).map { line =>
-        val Array(datasetId, startTime, endTime, value) = line.split(",").map(_.trim)
-        TimeSeriesData(datasetId.toInt, startTime, endTime, value.toDouble)
+        val parts = line.split(",").map(_.trim)
+        TimeSeriesData(parts(0).toInt, parts(1), parts(2), parts(3).toDouble)
       }.toList
     } finally {
       source.close()
@@ -119,9 +102,7 @@ object PowerAPI {
 
   def plotData(data: List[TimeSeriesData]): Unit = {
     val dataset = new DefaultCategoryDataset()
-    data.foreach { d =>
-      dataset.addValue(d.value, "MW/h", d.startTime)
-    }
+    data.foreach(d => dataset.addValue(d.value, "MW/h", d.startTime))
     val chart = ChartFactory.createBarChart(
       "Energy Generation Over Time",
       "Start Time",
@@ -133,7 +114,6 @@ object PowerAPI {
       false
     )
     ChartUtils.saveChartAsJPEG(new File("EnergyGenerationChart.jpeg"), chart, 800, 600)
-    println("Energy generation chart saved as EnergyGenerationChart.jpeg")
   }
 
   def detectIssues(data: List[TimeSeriesData]): Unit = {
@@ -146,12 +126,15 @@ object PowerAPI {
     }
   }
 
-  def sendRequest(apiKey: String, url: String, backend: SttpBackend[Identity, Any]): Response[Either[ResponseException[String, Error], ApiResponse]] = {
-    basicRequest
-      .header("x-api-key", apiKey)
-      .get(uri"$url")
-      .response(asJson[ApiResponse])
-      .send(backend)
+  def sendRequest(apiKey: String, url: String, backend: SttpBackend[Identity, Any]): Try[ApiResponse] = {
+    Try {
+      val response = basicRequest.header("x-api-key", apiKey).get(uri"$url").response(asJson[ApiResponse]).send(backend)
+      response.body match {
+        case Right(apiResponse) => apiResponse
+        case Left(error) => throw new Exception(error.toString)
+      }
+    }
   }
-  
+
+  private val lowOutputThreshold: Double = 200.0
 }
