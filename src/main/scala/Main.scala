5c7630b2e1b89e69652a69ef8b5c3c33b2d4541c
Trev-ops <152309481+Trev-ops@users.noreply.github.com>
2024-05-05 18:56:30 +0300

    Update Main.scala
    implemented 1st use case, will likely change later a bit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,10 +4,11 @@ import org.jfree.chart.{ChartFactory, ChartUtils}
 import org.jfree.data.category.DefaultCategoryDataset
 import sttp.client3._
 import sttp.client3.circe._
-import java.io.{File, PrintWriter}
+
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.{ChronoUnit, TemporalAdjusters}
+import java.io.{File, PrintWriter}
 import java.util.concurrent.{Executors, TimeUnit}
 import scala.io.{Source, StdIn}
 import scala.util.Try
@@ -17,21 +18,13 @@ case class ApiResponse(data: List[TimeSeriesData], pagination: Option[Pagination
 case class Pagination(currentPage: Int, lastPage: Int)
 
 object Main extends App {
-  def fetchDataAndStore(powerApi: PowerAPI, datasetId: String, formatter: DateTimeFormatter, now: ZonedDateTime): Try[Unit] = {
-    val oneMonthAgo = now.minus(1, ChronoUnit.MONTHS)
-    val dataTry = powerApi.fetchData(datasetId, formatter.format(oneMonthAgo), formatter.format(now))
-    dataTry.map { data =>
-      powerApi.storeData(data, s"${datasetId}Data.csv")
-    }
-  }
-
-  val now = ZonedDateTime.now()
-  val formatter = DateTimeFormatter.ISO_INSTANT
   val powerApi = new PowerAPI()
 
-  // Fetch and store data for datasets
-  val datasets = List("181", "191", "248")
-  datasets.foreach(datasetId => fetchDataAndStore(powerApi, datasetId, formatter, now))
+  val data = powerApi.fetchData("181", "2024-05-04T00:00:00Z", "2024-05-06T00:00:00Z")
+  data.foreach(powerApi.printData)
+  data.foreach(d => powerApi.storeData(d, "renewable_energy_data.csv"))
+  println("Data stored in renewable_energy_data.csv")
+  println()
 
   // Start the thread that reads the API every minute
   val executor = Executors.newSingleThreadScheduledExecutor()
@@ -42,7 +35,7 @@ object Main extends App {
         val now = java.time.LocalDateTime.now()
         val fifteenMinutesAgo = now.minusMinutes(15)
         val data = powerApi.fetchData("181", fifteenMinutesAgo.toString, now.toString)
-        data.foreach(d => powerApi.storeData(d, "181Data.csv"))
+        data.foreach(powerApi.printData)
       }
     }
   }
@@ -52,7 +45,7 @@ object Main extends App {
 
   @annotation.tailrec
   def menuLoop(): Unit = {
-    println("1. Current status\n" +
+    println("1. Monitor/Control\n" +
       "2. Collect data\n" +
       "3. View energy generation and storage\n" +
       "4. Analyse data\n" +
@@ -61,18 +54,12 @@ object Main extends App {
     print("Enter selection: ")
     scala.io.StdIn.readLine() match {
       case "1" =>
-        val datasets = Map("181" -> "Wind", "191" -> "Hydro", "248" -> "Solar")
-        datasets.foreach { case (datasetId, energyType) =>
-          val data = powerApi.readSecondLineFromFile(s"${datasetId}Data.csv")
-          data.foreach(line => {
-            val parts = line.split(",")
-            val timeSeriesData = TimeSeriesData(parts(0).toInt, parts(1), parts(2), parts(3).toDouble)
-            println(s"$energyType Production: ${timeSeriesData.value} MW/h at ${timeSeriesData.startTime}")
-          })
-        }
+        new EnergyController().controlEnergySource()
         println()
       case "2" =>
-        datasets.foreach(datasetId => fetchDataAndStore(powerApi, datasetId, formatter, now))
+        val data = powerApi.fetchData("181", "2024-05-04T00:00:00Z", "2024-05-06T00:00:00Z")
+        data.foreach(powerApi.printData)
+        data.foreach(d => powerApi.storeData(d, "renewable_energy_data.csv"))
         println("Data stored in renewable_energy_data.csv")
         println()
       case "3" =>
@@ -179,51 +166,101 @@ class PowerAPI {
     }
   }
 
-  def readLastLineFromFile(fileName: String): Option[String] = {
-    val source = Source.fromFile(fileName)
-    try {
-      val lines = source.getLines().toList
-      lines.lastOption
-    } catch {
-      case e: Exception =>
-        println(s"Error reading from file: $fileName")
-        None
-    } finally {
-      source.close()
-    }
-  }
-
-  def readFirstLineFromFile(fileName: String): Option[String] = {
-    val source = Source.fromFile(fileName)
-    try {
-      val lines = source.getLines().toList
-      lines.headOption
-    } catch {
-      case e: Exception =>
-        println(s"Error reading from file: $fileName")
-        None
-    } finally {
-      source.close()
-    }
-  }
-
-  def readSecondLineFromFile(fileName: String): Option[String] = {
-    val source = Source.fromFile(fileName)
-    try {
-      val lines = source.getLines().toList
-      lines.lift(1)
-    } catch {
-      case e: Exception =>
-        println(s"Error reading from file: $fileName")
-        None
-    } finally {
-      source.close()
-    }
-  }
-
   private val lowOutputThreshold: Double = 200.0
 }
 
+// For the 1st option in the menu
+class EnergyController {
+  val solarPanelPosition: Int = 1   // In real application would read the position from a sensor and update properly
+  val windTurbinePosition: Int = 1  // Just added to simulate how it would look like
+  val hydropowerPosition: Int = 1
+
+  def controlEnergySource(): Unit = {
+    println("Please enter the energy source you wish to view (solar, wind, hydro): ")
+    val energyType = scala.io.StdIn.readLine().toLowerCase
+    energyType match {
+      case "solar" | "wind" | "hydro" =>
+        val fileName = energyType match {
+          case "solar" => "renewable_energy_data2.csv"
+          case "wind" => "renewable_energy_data.csv"
+          case "hydro" => "hydro_energy_data.csv"
+        }
+        val (startTime, endTime) = ReadStartAndEndDates(fileName)
+        println(s"The $energyType energy source started collecting energy on $startTime.")
+        println(s"New data was last added to the $energyType energy source on $endTime.")
+
+        val currentPosition = energyType match {
+          case "solar" => solarPanelPosition
+          case "wind" => windTurbinePosition
+          case "hydro" => hydropowerPosition
+        }
+        println(s"The current position of $energyType is $currentPosition. Do you want to change the position? (yes/no)")
+        scala.io.StdIn.readLine().toLowerCase match {
+          case "yes" =>
+            def askForNewPosition(): Unit = {
+              println("Please enter the new position (1-5) or 'abort' to cancel: ")
+              val newPosition = scala.io.StdIn.readLine().toLowerCase
+              newPosition match {
+                case "abort" => return
+                case "1" | "2" | "3" | "4" | "5" =>
+                  if (newPosition.toInt == currentPosition) {
+                    println(s"The $energyType is already in position $newPosition. Please try again.")
+                    askForNewPosition()
+                  } else {
+                    energyType match {
+                      case "solar" => moveSolarPanel(newPosition.toInt)
+                      case "wind" => moveWindTurbine(newPosition.toInt)
+                      case "hydro" => moveHydropower(newPosition.toInt)
+                    }
+                  }
+                case _ =>
+                  println("Invalid position.")
+                  askForNewPosition()
+              }
+            }
+            askForNewPosition()
+          case "no" => println(s"The position of $energyType remains at $currentPosition.")
+          case _ =>
+            println("Invalid response.")
+            controlEnergySource()
+        }
+      case _ =>
+        println("Invalid energy type. Please enter either 'solar', 'wind', or 'hydro'.")
+        controlEnergySource()
+    }
+  }
+
+  def ReadStartAndEndDates(fileName: String): (String, String) = {
+    val source = scala.io.Source.fromFile(fileName)
+    val lines = source.getLines().drop(1).toList
+    source.close()
+    val firstLine = lines(0)
+    val lastLine = lines.last
+    val originalStartDate = lastLine.split(",")(1)
+    val originalEndDate = firstLine.split(",")(1)
+    val zonedStartDate = ZonedDateTime.parse(originalStartDate)
+    val zonedEndDate = ZonedDateTime.parse(originalEndDate)
+    val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+    (zonedStartDate.format(formatter), zonedEndDate.format(formatter))
+  }
+
+  def moveSolarPanel(newPosition: Int): Unit = {
+    // solarPanelPosition = newPosition
+    println(s"The position of the solar panel has been changed to $newPosition.")
+  }
+
+  def moveWindTurbine(newPosition: Int): Unit = {
+    // windTurbinePosition = newPosition
+    println(s"The position of the wind turbine has been changed to $newPosition.")
+  }
+
+  def moveHydropower(newPosition: Int): Unit = {
+    // hydropowerPosition = newPosition
+    println(s"The position of the hydropower has been changed to $newPosition.")
+  }
+}
+
+// For the 4th option in the menu
 class AnalyzeData { // Could add that user can choose to analyze data from a start and end date
   def EnergyType(): String = {
     println("Please enter the type of energy to analyze (solar, wind, hydro): ")
@@ -242,16 +279,16 @@ class AnalyzeData { // Could add that user can choose to analyze data from a sta
     val interval = StdIn.readLine().toLowerCase
 
     val fileName = energyType match {
-      case "solar" => "248Data.csv"
-      case "wind" => "181Data.csv"
-      case "hydro" => "191Data.csv"
+      case "solar" => "renewable_energy_data2.csv" // Need to change when have solar data
+      case "wind" => "renewable_energy_data.csv"
+      case "hydro" => "hydro_energy_data.csv" // Need to change when have hydro data
     }
 
-    val data = analyze(interval, fileName)
+    val data = Analyze(interval, fileName)
     printAnalysis(data, interval)
   }
 
-  def analyze(interval: String, fileName: String): List[TimeSeriesData] = {
+  def Analyze(interval: String, fileName: String): List[TimeSeriesData] = {
     val source = scala.io.Source.fromFile(fileName)
     try {
       val lines = source.getLines().drop(1)
@@ -323,7 +360,5 @@ class AnalyzeData { // Could add that user can choose to analyze data from a sta
     } else {
       println("No data to analyze.")
     }
-
-
   }
 }
